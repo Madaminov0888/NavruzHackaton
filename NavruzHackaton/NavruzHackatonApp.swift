7204389bf6beefdeb69655e1d8f7e0d7318af39a
Madaminov Muhammadjon <90606773+Madaminov0888@users.noreply.github.com>
2025-03-18 10:17:26 +0500

    Initial Commit

---
+++ NavruzHackaton/NavruzHackatonApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavruzHackatonApp.swift
+//  NavruzHackaton
+//
+//  Created by Muhammadjon Madaminov on 18/03/25.
+//
+
+import SwiftUI
+
+@main
+struct NavruzHackatonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
